f9fe8101a88fd751dc16bd34860e8ece9aa63b0b
IlaiSheffer <ilai.sheffer@gmail.com>
2025-12-31 12:12:43 +0200

    add aspNet file

---
+++ myFirstProject/AspNet.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="AspNet.aspx.cs" Inherits="AspNet" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h2>Asp.Net</h2>
+    <%Response.Write(str); %>
+    <%Response.Write(str2); %>
+    <%Response.Write(kefel); %>
+</asp:Content>
+
---
+++ myFirstProject/AspNet.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AspNet : System.Web.UI.Page
+{
+    public string str;
+        public string str2;
+    public string kefel;
+
+        protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write("<font style='font-size:30px'>Hello</font>");
+        str = "<font style='font-size:30px'>Hello</font>";
+
+        for(int i=1; i<=10; i++)
+        {
+            Response.Write("<font style='font-size:30px'>" + i + "</font>");
+        }
+
+        str2 = "";
+        for(int i=1; i<=10; i++)
+        {
+            str2 += "<font style='font-size:30px'>" + i + "</font>";
+            str2 += "<br/>";
+        }
+        kefel = "";
+        kefel = "<table border='1'>";
+        for(int i=1; i<=10; i++)
+        {
+            kefel += "<tr>";
+            
+            for(int j=1; j<=10; j++)
+            {
+                kefel += "<td>";
+                kefel += i * j;
+                kefel += "<td/>";
+            }
+            kefel += "<tr/>";
+        }
+        kefel += "</table>";
+    }
+   
+}
